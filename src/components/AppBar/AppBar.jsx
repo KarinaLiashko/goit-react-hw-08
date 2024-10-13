a0bf829ff7a1806ebfd3dd11069dd2b59c252934
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2024-10-13 14:53:30 +0200

    AppBarr, UserMenu

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,23 +1,17 @@
-import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navigation from "../Navigation/Navigation";
+import AuthNav from "../AuthNav/AuthNav";
+import UserMenu from "../UserMenu/UserMenu";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import styles from "./AppBar.module.css";
 
 const AppBar = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <header className={styles.header}>
-      <nav>
-        <NavLink className={styles.navLink} to="/">
-          Home
-        </NavLink>
-        <NavLink className={styles.navLink} to="/register">
-          Register
-        </NavLink>
-        <NavLink className={styles.navLink} to="/login">
-          Login
-        </NavLink>
-        <NavLink className={styles.navLink} to="/contacts">
-          Contacts
-        </NavLink>
-      </nav>
+      <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </header>
   );
 };
